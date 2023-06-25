eaec248dc3f155131c5b2fa82a9242e97f4e5221
Aninda Nath <nathaninda01@gmail.com>
2023-06-25 08:17:38 +0530

    add view to normalKeys

--- src/components/keypad.js
+++ src/components/keypad.js
@@ -2,17 +2,17 @@ import style from './keypad.module.css';
 import Button from './button'
 
 const normalKeys = [
-    [{ ques: <>7</>, evalue: '7' }, { ques: <>4</>, evalue: '4' }, { ques: <>1</>, evalue: '1' }, { ques: <>0</>, evalue: '0' }],
-    [{ ques: <>8</>, evalue: '8' }, { ques: <>5</>, evalue: '5' }, { ques: <>2</>, evalue: '2' }, { ques: <>.</>, evalue: '.' }],
-    [{ ques: <>9</>, evalue: '9' }, { ques: <>6</>, evalue: '6' }, { ques: <>3</>, evalue: '3' }, { ques: <>=</>, evalue: '=' }],
-    [{ ques: <>&times;</>, evalue: '*' }, { ques: <>+</>, evalue: '+' }, { ques: <>&divide;</>, evalue: '/' }, { ques: <>-</>, evalue: '-' }]
+    [{ ques: <>7</>, view : <>7</>, evalue: '7' }, { ques: <>4</>, view : <>4</>, evalue: '4' }, { ques: <>1</>, view : <>1</>, evalue: '1' }, { ques: <>0</>, view : <>0</>, evalue: '0' }],
+    [{ ques: <>8</>, view : <>8</>, evalue: '8' }, { ques: <>5</>, view : <>5</>, evalue: '5' }, { ques: <>2</>, view : <>2</>, evalue: '2' }, { ques: <>.</>, view : <>.</>, evalue: '.' }],
+    [{ ques: <>9</>, vieew : <>9</>, evalue: '9' }, { ques: <>6</>, view : <>6</>, evalue: '6' }, { ques: <>3</>, view : <>3</>, evalue: '3' }, { ques: <>Exp&#40;</>, view : <>&times;e^&#40;</>, evalue: '*e^(' }],
+    [{ ques: <>DEL</>, view : <></>, evalue: 'DEL' }, { ques: <>&times;</>, view : <>&times</>, evalue: '*' }, { ques: <>+</>, view : <>+</>, evalue: '+' }, { ques: <>Ans</>, view : <>Ans</>, evalue: 'Ans' }],
+    [{ ques: <>AC</>, view : <></>, evalue: 'AC' }, { ques: <>&divide;</>, view : <>&divide;</>, evalue: '/' }, { ques: <>-</>, view : <>-</>, evalue: '-' }, { ques: <>=</>, view : <>=</>, evalue: '=' }]
 ]
 
 const specialKeys = [
     [{ ques :<>&#40;</> , evalue : '('}, { ques : <>&#41;</>, evalue : ')' }, { ques : <>e</>, evalue : '(e)'}, { ques : <>&pi;</>, evalue : '(pi)' }],
-    [{ ques : <>sin&#40;</>, evalue : 'sin(' }, { ques : <>cos&#40;</>, evalue : 'cos('}, { ques : <>tan&#40;</>, evalue : 'tan(' }, { ques: <>Ans</>, evalue: 'Ans' }],
-    [{ ques :<>^&#40;</> , evalue : '^('}, { ques : <>log</>, evalue : 'log' }, { ques : <>log10&#40;</>, evalue : 'log10('}, { ques: <>DEL</>, evalue: 'DEL' }],
-    [{ ques :<>abs&#40;</> , evalue : 'abs('}, { ques: <>Exp&#40;</>, evalue: '*e^(' }, { ques : <>&times;10&#40;</>, evalue : '*10^(' }, { ques: <>AC</>, evalue: 'AC' }]
+    [{ ques :<>abs&#40;</> , evalue : 'abs('}, { ques : <>sin&#40;</>, evalue : 'sin(' }, { ques : <>cos&#40;</>, evalue : 'cos('}, { ques : <>tan&#40;</>, evalue : 'tan(' }],
+    [{ ques :<>^&#40;</> , evalue : '^('}, { ques : <>log10&#40;</>, evalue : 'log10(' }, { ques : <>ln&#40;</>, evalue : 'log('}, { ques : <>&times;10&#40;</>, evalue : '*10^(' }]
 ]
 
 const Keypad = (props) => {
@@ -23,8 +23,8 @@ const Keypad = (props) => {
     const Pad = ( keypad ) => keypad.map((col, ind) => {
         return (
             <div className={style.column} key={ind}>
-                {col.map(({ ques, evalue }) => {
-                    return <Button key={evalue} value={{q : ques , val : evalue}} screenShow={screenShow}>
+                {col.map(({ ques, view, evalue }) => {
+                    return <Button key={evalue} value={{q : view , val : evalue}} screenShow={screenShow}>
                         {ques}
                     </Button>
                 })}
